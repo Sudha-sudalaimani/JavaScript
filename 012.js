82a899cc8558d6b6f3295e15a7d3b970dd7df2fd
Sudha <s.sudha19082006@gmail.com>
2025-10-01 13:51:59 +0530

    Create 012.js

---
+++ 012.js
@@ -0,0 +1,16 @@
+//For Loop
+for(i=10;i>=1;i=i-1){
+    console.log(i)
+}
+
+//print even numbers 1 to 10
+for(i=1;i<=10;i=i+1){
+    if(i%2==0){
+        console.log(i)
+    }
+}
+
+//2 table
+for(i=1;i<=10;i=i+1){
+    console.log(`${i} X 2 = ${i*2}`)
+}
