c6893917c7ad38a16cfaaa6d74334b69838515fc
Sudha <s.sudha19082006@gmail.com>
2025-10-01 13:50:47 +0530

    Create 009.js

---
+++ 009.js
@@ -0,0 +1,12 @@
+//Function with return type
+function myname(){
+    return "Sam"
+}
+var name1=myname()
+console.log(name1)
+//with parameter and return type
+function add(a,b){
+    return `Sum: ${a+b}`
+}
+var sum=add(10,200)
+console.log(sum)
