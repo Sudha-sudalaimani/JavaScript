2a4ee3a390ddabc952ed181c7caf397d1dc8ca51
Sudha <s.sudha19082006@gmail.com>
2025-10-01 13:51:07 +0530

    Create 010.js

---
+++ 010.js
@@ -0,0 +1,43 @@
+//if-else Q1
+var rain=false
+if (rain==true){
+    console.log("Take an umbrella")
+}else{
+    console.log("Enjoy the sunlight")
+}
+
+//Q2
+var hw="yes"
+if (hw=="yes"){
+    console.log('Great Job')
+}
+else{
+    console.log("Finish your homework before playing")
+}
+
+//Q3
+let cookie="yes";
+if(cookie=="yes"){
+    console.log("would you like cookies!")
+}
+else{
+    console.log("Time to bake more cookies")
+}
+
+//Q4 did you brush your teeth?
+var brush="no";
+if(brush=="yes"){
+    console.log("Your teeth are clean ")
+}
+else{
+    console.log("Go brush your teeth")
+}
+
+//Q5
+var birthday="yes"
+if (birthday=="yes"){
+    console.log("Wish you Happy birthday!")
+}
+else{
+    console.log("Have a good day")
+}
