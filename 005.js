24472e6b0b7be5dec4bfe6421e43117623cd2e80
Sudha <s.sudha19082006@gmail.com>
2025-10-01 13:49:02 +0530

    Create 005.js

---
+++ 005.js
@@ -0,0 +1,20 @@
+function samsung(){
+    console.log("This is samsung mobile!");
+}
+samsung()
+
+function oppo(){
+    console.log("This is oppo Phone");
+}
+oppo()
+
+function iphone()
+{
+    console.log("This is IPHONE 17")
+}
+iphone()
+
+function redmi(){
+    console.log("This is redmi mobile")
+}
+redmi()
