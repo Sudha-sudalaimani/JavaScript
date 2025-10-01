dadfd8f5005f03fdea6adaed474da052d9f45289
Sudha <s.sudha19082006@gmail.com>
2025-10-01 13:49:56 +0530

    Create 007.js

---
+++ 007.js
@@ -0,0 +1,11 @@
+var factor="Suriya";
+var fplayer="Dhoni"
+var fmovie="Dragon"
+
+function favourite()
+{
+    console.log(`Favourite Actor : ${factor}`)
+    console.log(`Favourite Player : ${fplayer}`)
+    console.log(`Favourite Movie : ${fmovie}`)
+}
+favourite()
