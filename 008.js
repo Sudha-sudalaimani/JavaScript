38a16508dcf27131eda9a7ff00a01e9a0348993f
Sudha <s.sudha19082006@gmail.com>
2025-10-01 13:50:18 +0530

    Create 008.js

---
+++ 008.js
@@ -0,0 +1,13 @@
+function add(a,b){
+    console.log("Sum: ",a+b);
+}
+add(10,20)
+
+//Exercise
+function area(length,breadth){
+    var result=length*breadth;
+    console.log("AREA: "+result);
+}
+var l=10
+var  b=30
+area(l,b)
