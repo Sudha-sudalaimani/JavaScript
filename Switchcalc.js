83678bfe4325473085356ee5c1aa16ebd1a70261
Sudha <s.sudha19082006@gmail.com>
2025-10-02 11:28:06 +0530

    Create Switchcalc.js

---
+++ Switchcalc.js
@@ -0,0 +1,23 @@
+//switch case
+
+let a=10;
+let b=20;
+let operator="*"
+switch(operator){
+    case "+":
+        console.log(a+b);
+        break;
+    case "-":
+        console.log(a-b);
+        break;
+    case "*":
+        console.log(a*b);
+        break;
+    case "/":
+        console.log(a/b);
+        break;
+    default:
+        console.log("invalid Operator");
+}
+
+
